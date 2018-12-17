7df4ca87b2bf9279a3978425ca8b29291e50f3b7
Alp Tezbasaran <alptezbasaran@gmail.com>
2018-12-17 15:40:48 -0500

    initial commit 3

--- ctfPostProcessor.py
+++ ctfPostProcessor.py
@@ -1,52 +1,58 @@
-"""
+'''
 Created on Dec 13, 2018
 
 @author: alptezbasaran
-"""
+'''
 
 import math
 import os
 import sys
 import matplotlib.pyplot as py
 
-class postProcessor:
-	"""
-	Information
-	"""
-	def inputs():
-		"""
-		Define plotting and exctraction parameters
-		"""
-		timeDependent = False
-		channelNumber = 1
+# Unit conversion factors
+t_in_m=0.0254
+t_BTU_kJ=1.05506
+t_MJ_kJ=1000.0
+t_kJ_MJ=1/t_MJ_kJ
+t_hr_s=3600.0
+t_ft_m=0.3048
 
-		print('inputs')
 
-	def readChannelData():
-		"""
-		Reads data from specified file
-		"""
-		print('channelData')
+class channelData:
+  '''
+  Reads data from specified file
+  '''
+  def __init__(self, channelsOutputFileName):
+    '''
+    '''
+    self.channelsOutputFile = channelsOutputFileName
+    print('ayakChannels')
+    print(self.channelsOutputFile)
 
-	def readMajorData():
-		"""
-		Reads data from specified file
-		"""
-		print('majorData')
+class majorData:
+  '''
+  Reads data from *.ctf.out
+  Converts British units to SI if asked
+  '''
+  def __init__(self,majorOutputFileName):
+    '''
+    @In - major putput file name
+    '''
+    self.majorOutputFile = majorOutputFileName
+    print('ayakMajor')
+    print(self.majorOutputFile)
+    
 
-	def plot():
-		"""
-		Plotting function
-		"""
-		print('plotting')
+    self.lines = open(majorOutputFileName).readlines()
+    # To invoke information in each time step reader functions are invoked
+    # But first time information should be extracted
+    # A default option to extract data at the last step must be added
 
-	def writeToFile():
-		print('writingToFile')
 
-	def __init__():
-		"""
 
-		"""
-		print('Ayak')
+# To run as a script
+if __name__ == "__main__":
 
-		
+  inputName = 'ctfM_0_dropletInjection'
+  channel = channelData(inputName + '.ctf.channels.out')
+  major   = majorData(inputName + '.ctf.out')
